2938ccdeb9d855b6a98bc2ed3203c2a254cdfcc3
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2023-01-16 19:08:10 +0100

    118

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:pelisapp/providers/movie_provider.dart';
 import 'package:pelisapp/screens/screens.dart';
+import 'package:provider/provider.dart';
 
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+        ChangeNotifierProvider(create: ( _ ) => MovieProvider(), lazy: false,)
+    ],
+    child: const MyApp(),
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
